58839d36eba1176bd25543973b119591db741312
dpetrov <dpetrov@mabius.ru>
2016-10-20 15:36:48 +0300

    Create first version of project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wai-logging-effect-google
+
+Middleware for [wai](https://www.stackage.org/lts-7.4/package/wai-3.2.1.1) which log to 
+[Google Cloud Platform's](https://cloud.google.com/) [Logging](https://cloud.google.com/logging/docs/) service.
+
+
+[] Add field latency when @brendanhay update [gogol-logging](https://github.com/brendanhay/gogol)
---
+++ src/Network/Wai/Logger/GoogleLogging.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.Wai.Logger.GoogleLogging where
+
+import Data.IP (fromHostAddress, fromIPv4, fromIPv6, fromHostAddress6)
+import Network.Socket (SockAddr(..), HostAddress6, HostAddress)
+import GHC.Int (Int64)
+import Network.Google.Logging (LogEntry)
+import Network.HTTP.Types.Status (Status(..))
+import Control.Lens ((&), (.~))
+import Data.Text (Text)
+import Data.Time
+       (UTCTime, getCurrentTime, formatTime, defaultTimeLocale,
+        diffUTCTime, NominalDiffTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Control.Monad.Log (Handler)
+import Network.Wai (Middleware, Request, Response, RequestBodyLength(..))
+
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Network.Wai as Wai
+import qualified Network.Google.Logging as Logging
+
+
+loggerMiddleware
+    :: Handler IO LogEntry -> Middleware
+loggerMiddleware logger app req respond = do
+    requestTime <- getCurrentTime
+    app req $
+        \res -> do
+            responseTime <- getCurrentTime
+            _ <-
+                logger
+                    (fillLogEntry
+                         (responseTime `diffUTCTime` requestTime)
+                         responseTime
+                         req
+                         res)
+            respond res
+
+-- | Helper function for fill LogEntry fields with data from
+-- Request and Response
+fillLogEntry :: NominalDiffTime -> UTCTime -> Request -> Response -> LogEntry
+fillLogEntry lat currentTime req res =
+    (((Logging.logEntry & Logging.leHTTPRequest .~ (pure fillHTTPRequest))
+                        & Logging.leSeverity .~ (pure (statusSeverity (Wai.responseStatus res))))
+                        & Logging.leTimestamp .~ (pure (showTimestamp currentTime)))
+  where
+    fillHTTPRequest =
+        ((((((((Logging.hTTPRequest) & Logging.httprReferer .~ (Text.decodeUtf8 <$> Wai.requestHeaderReferer req))
+                                     & Logging.httprRemoteIP .~ (pure (ipFromSockAddr (Wai.remoteHost req))))
+                                     & Logging.httprUserAgent .~ (Text.decodeUtf8 <$> Wai.requestHeaderUserAgent req))
+                                     & Logging.httprStatus .~ (pure (fromIntegral (statusCode (Wai.responseStatus res)))))
+                                     & Logging.httprRequestSize .~ (pure (rblToInt (Wai.requestBodyLength req))))
+                                     & Logging.httprRequestURL .~ (pure (Text.decodeUtf8 (Wai.rawPathInfo req))))
+                                     & Logging.httprRequestMethod .~ (pure (Text.decodeUtf8 (Wai.requestMethod req))))
+
+
+-- | Helper function for extract client IP address
+ipFromSockAddr :: SockAddr -> Text
+ipFromSockAddr (SockAddrInet _ ha) = showIPv4 ha
+ipFromSockAddr (SockAddrInet6 _ _ ha _) = showIPv6 ha
+ipFromSockAddr (SockAddrUnix sock) = Text.pack sock
+ipFromSockAddr (SockAddrCan i) = Text.pack (show i)
+
+-- | A timestamp in RFC3339 UTC "Zulu" format, accurate to nanoseconds.
+-- Example: "2014-10-02T15:01:23.045123456Z".
+showTimestamp :: UTCTime -> Text
+showTimestamp =
+    Text.pack . formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S.%Q%Z"
+
+
+-- | Define log severity depend on response status.
+-- when 1xx then INFO
+--      2xx - DEFAULT
+--      3xx - NOTICE
+--      4xx - ERROR
+--      5xx - CRITICAL
+statusSeverity :: Status -> Text
+statusSeverity (Status code _) =
+    if code < 200
+        then "INFO"
+        else if (code < 300)
+                 then "DEFAULT"
+                 else if (code < 400)
+                          then "NOTICE"
+                          else if (code < 500)
+                                   then "ERROR"
+                                   else "CRITICAL"
+
+-- | Conver Wai RequestBodyLength to Integer
+-- when rbl is chuncked body we set size to 0 another return known length
+rblToInt :: RequestBodyLength -> Int64
+rblToInt ChunkedBody = 0
+rblToInt (KnownLength kl) = fromIntegral kl
+
+-- | Grab from <https://hackage.haskell.org/package/wai-extra-3.0.17/docs/src/Network-Wai-Middleware-RequestLogger-JSON.html#formatAsJSON here>
+-- because don't want add wai-extra dependencies for one function
+showIPv4 :: HostAddress -> Text
+showIPv4 =
+    Text.intercalate "." . map (Text.pack . show) . fromIPv4 . fromHostAddress
+
+showIPv6 :: HostAddress6 -> Text
+showIPv6 =
+    Text.intercalate ":" . map (Text.pack . show) . fromIPv6 . fromHostAddress6
+
+-- | A duration in seconds with up to nine fractional digits,
+-- terminated by 's'. Example: "3.5s".
+showDuration :: NominalDiffTime -> Text
+showDuration =
+    Text.pack . formatTime defaultTimeLocale "%s%Qs" . posixSecondsToUTCTime
